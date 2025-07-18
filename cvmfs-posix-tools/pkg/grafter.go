d30e46ec8107015ac63e27bff309351351f8d2eb
Andriy Utkin <hello@autkin.net>
2025-07-18 20:08:05 +0100

    Add cvmfs-posix-tools
    These are tools which offer the convenience of direct effect on CVMFS
    repo.
    
    These tools require "cvmfs_swissknife ingestsql" to be available.
    ingestsql manipulates repo state through cvmfs_gateway.
    
    One tool is named cvmfs_rsync. There is another script in the git repo
    called cvmfs_rsync. Please don't confuse them.

---
+++ cvmfs-posix-tools/pkg/grafter.go
@@ -0,0 +1,171 @@
+package pkg
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+type GraftOptions struct {
+	PriorityVal int64
+	Debug       bool
+	Priority    string
+}
+
+// Converts the priority value to the associated priority string, lease retry interval, and start offset
+func PriorityToPriorityInfo(priority string) (string, int64) {
+	priorityVal := LowPriorityVal
+	switch priority {
+	case HighPriority:
+		priorityVal = HighPriorityVal
+	case MedPriority:
+		priorityVal = MedPriorityVal
+	case LowPriority:
+		fallthrough
+	default:
+		priorityVal = LowPriorityVal
+	}
+	return priority, int64(priorityVal*1.e6) - time.Now().Unix()
+}
+
+type GraftMetrics struct {
+	Delta                         float64
+	Files, Dirs, Links, Deletions int
+	Priority                      string
+	LeasePath                     string
+	Revision                      string
+}
+
+// Perform a graft with the passed in db using the currently loaded cvmfs_swissknife module
+func NewGraftOptions(priority string) GraftOptions {
+	// initialize the option with sensitive default
+	priorityString, priorityVal := PriorityToPriorityInfo(priority)
+	log.Info().Str("Priority", priorityString).Msg("Processing graft with the given priority.")
+	return GraftOptions{
+		PriorityVal: priorityVal,
+		Debug:       false,
+		Priority:    priorityString,
+	}
+}
+
+func GraftWithOptions(db DB, repo string, options GraftOptions) (graftingMetrics GraftMetrics, err error) {
+	numFiles, numDirs, numLinks, numDels, err := db.DBCounts()
+	graftingMetrics = GraftMetrics{Files: numFiles, Dirs: numDirs, Links: numLinks, Deletions: numDels, Priority: options.Priority, LeasePath: MissingMetric, Revision: MissingMetric}
+	stdoutExtractions := map[string]*regexp.Regexp{LeasePathRegexName: regexp.MustCompile(LeasePathRegex), RevisionRegexName: regexp.MustCompile(RevisionRegex)}
+	stdoutExtracted := map[string][]string{}
+	log.Info().Msg("Grafting:")
+	start_time := time.Now()
+	defer func() {
+		end_time := time.Now()
+		graftingMetrics.Delta = end_time.Sub(start_time).Seconds()
+		log.Info().Float64("delta (s)", graftingMetrics.Delta).Msg("Grafting Time")
+	}()
+	var cvmfsRsyncTempDir string
+	cvmfsRsyncTempDir, err = os.MkdirTemp("", "cvmfs_rsync_swissknife")
+	if err != nil {
+		return
+	}
+	defer func() {
+		if tempErr := os.RemoveAll(cvmfsRsyncTempDir); tempErr != nil {
+			log.Error().Err(tempErr).Msg("Error in cleanup of tempDir")
+			if err == nil {
+				err = tempErr
+			}
+		}
+	}()
+
+	log.Debug().Int64("Priority Value", options.PriorityVal).Msg("Calculated Priority")
+
+	var args = []string{
+		"ingestsql",
+		"-N", repo, /* fully qualified repository name */
+		"-D", db.GetPath(), /* input sqlite DB */
+		"-t", cvmfsRsyncTempDir, /* temporary directory */
+		"-a",                                          /* Allow additions */
+		"-d",                                          /* Allow deletions */
+		"-B", fmt.Sprintf("/cvmfs/%s", repo), /* mount point to block on pending visibility of update */
+		"-P", strconv.FormatInt(options.PriorityVal, 10),
+	}
+
+	cmd := exec.Command("cvmfs_swissknife", args...)
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return
+	}
+	// Get logging scanners
+	stdoutIn := bufio.NewScanner(stdout)
+	stderrIn := bufio.NewScanner(stderr)
+
+	if err = cmd.Start(); err != nil {
+		return
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func(scanner *bufio.Scanner) {
+		defer wg.Done()
+		stdoutExtracted = LeveledPipeLoggerWithExtraction(scanner, zerolog.InfoLevel, stdoutExtractions)
+	}(stdoutIn)
+	wg.Add(1)
+	go func(scanner *bufio.Scanner) {
+		defer wg.Done()
+		LeveledPipeLogger(scanner, zerolog.WarnLevel)
+	}(stderrIn)
+
+	wg.Wait()
+	if err = cmd.Wait(); err != nil {
+		log.Error().Err(err).Str("Swissknife command", cmd.String()).Msg("Error in grafting")
+		return
+	}
+
+	if match, ok := stdoutExtracted[LeasePathRegexName]; ok {
+		graftingMetrics.LeasePath = match[1]
+	}
+	if match, ok := stdoutExtracted[RevisionRegexName]; ok {
+		graftingMetrics.Revision = match[1]
+	}
+
+	log.Info().Msg("Finished Grafting")
+
+	time.Sleep(PreSyncDelaySeconds * time.Second)
+
+	return
+}
+
+func Graft(db DB, repo string, priority string, debug bool) (graftingMetrics GraftMetrics, err error) {
+	options := NewGraftOptions(priority)
+	options.Debug = debug
+	return GraftWithOptions(db, repo, options)
+}
+
+// Remount and sync a repository (currently doesn't actually ensure sync afterwards)
+func RemountSyncRepo(repo string, debug bool) error {
+	cmd := exec.Command("sudo", "-u", "cvmfs", "/bin/cvmfs_talk", "-i", repo, "remount", "sync")
+
+	if debug {
+		cmd.Stdout = log.Logger
+		cmd.Stderr = log.Logger
+	}
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+	if err := cmd.Wait(); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ cvmfs-posix-tools/pkg/mock_grafter.go
@@ -0,0 +1,106 @@
+package pkg
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	pathlib "github.com/chigopher/pathlib"
+	"github.com/rs/zerolog/log"
+)
+
+// Get a mock grafter (Needs to match graft function signature)
+func Mock_graft_getter() func(db DB, repo string, priority string, debug bool) (GraftMetrics, error) {
+	absPath, err := GetAbsolutePath(pathlib.NewPath(CurrentDirectory))
+	if err != nil {
+		panic(err)
+	}
+	return func(db DB, repo string, priority string, debug bool) (GraftMetrics, error) {
+		return Mock_graft(db, repo, debug, absPath.Parent().Parent().Clean().String())
+	}
+}
+
+func Mock_graft_getter_options() func(db DB, repo string, options GraftOptions) (GraftMetrics, error) {
+	absPath, err := GetAbsolutePath(pathlib.NewPath(CurrentDirectory))
+	if err != nil {
+		panic(err)
+	}
+	return func(db DB, repo string, options GraftOptions) (GraftMetrics, error) {
+		return Mock_graft(db, repo, options.Debug, absPath.Parent().Parent().Clean().String())
+	}
+}
+
+// Perform a graft to the test mount
+func Mock_graft(db DB, repo string, debug bool, baseDir string) (GraftMetrics, error) {
+	log.Info().Msg("Grafting:")
+	cvmfsRsyncTempDir, err := os.MkdirTemp("", "cvmfs_rsync_swissknife")
+	if err != nil {
+		return GraftMetrics{}, err
+	}
+	defer os.RemoveAll(cvmfsRsyncTempDir)
+	proxy := "http://127.0.0.1:6081"
+	configPrefix := "/etc/cvmfs/gateway-client/test.repo/"
+	args_add := []string{"-B", TestMount()}
+	if !FullyContainerized() {
+		proxy = "DIRECT"
+		configPrefix = baseDir + "/pkg/etc/cvmfs-gateway-client/test.repo/"
+		args_add = []string{}
+	}
+	args := append([]string{"ingestsql", "-@", proxy, "-N", "test.repo",
+		"-D", db.GetPath(), "-w", "http://127.0.0.1:8000/test.repo/test.repo", "-k", configPrefix + "pubkey",
+		"-s", configPrefix + "gatewaykey", "-3", configPrefix + "s3.conf", "-g", "http://127.0.0.1:4929/api/v1",
+		"-t", cvmfsRsyncTempDir, "-a", "-d"}, args_add...)
+
+	cmd := exec.Command("cvmfs_swissknife", args...)
+
+	fmt.Println(cmd.String())
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+	if err := cmd.Start(); err != nil {
+		return GraftMetrics{}, err
+	}
+	if err := cmd.Wait(); err != nil {
+		return GraftMetrics{}, err
+	}
+
+	log.Info().Msg("Finished Grafting")
+
+	return GraftMetrics{}, nil
+}
+
+// Mock it out so that if graft is called, an error is thrown
+func Mock_no_graft(db DB, repo string, priority string, debug bool) (GraftMetrics, error) {
+	panic(fmt.Errorf("nothing should be grafted so this shouldn't be called"))
+}
+
+// Mock it out so that if graft is called, an error is thrown
+func Mock_no_graft_options(db DB, repo string, options GraftOptions) error {
+	panic(fmt.Errorf("nothing should be grafted so this shouldn't be called"))
+}
+
+// Return a grafting function that doesn't graft, but instead copies the databse into another database to be grafted later
+func Mock_mega_graft(graftDb DB) func(db DB, repo string, priority string, debug bool) (GraftMetrics, error) {
+	return func(db DB, repo string, priority string, debug bool) (GraftMetrics, error) {
+		log.Info().Msg("Grafting:")
+		err := graftDb.CopyInDatabase(db)
+		if err != nil {
+			return GraftMetrics{}, err
+		}
+		log.Info().Msg("Finished Grafting")
+		return GraftMetrics{}, nil
+	}
+}
+
+// Return a grafting function that doesn't graft, but instead copies the databse into another database to be grafted later
+func Mock_mega_graft_options(graftDb DB) func(db DB, repo string, options GraftOptions) (GraftMetrics, error) {
+	return func(db DB, repo string, options GraftOptions) (GraftMetrics, error) {
+		log.Info().Msg("Grafting:")
+		err := graftDb.CopyInDatabase(db)
+		if err != nil {
+			return GraftMetrics{}, err
+		}
+		log.Info().Msg("Finished Grafting")
+		return GraftMetrics{}, nil
+	}
+}
