687fb8e6d5f1947cd2e856a8f31dd3e7f91fdbd2
Andriy Utkin <hello@autkin.net>
2026-03-13 11:46:39 +0000

    ci.sh: add flavour to container & image names
    So that multiple workcopies can be tested simultaneously and not
    replace each other's images and containers
    
    The distinguishing "flavour" is repo root dir's parent dir, but can be
    redefined by setting FLAVOUR env var when launching ci.sh.
    The default serves best the case when the user runs multiple workcopies
    in the dir layout like
    - /usr/local/src/devel/cvmfs
    - /usr/local/src/feature-1/cvmfs
    - /usr/local/src/feature-2/cvmfs

--- test/common/container/ci.sh
+++ test/common/container/ci.sh
@@ -7,11 +7,17 @@ if ! [[ -d /sys/module/overlay ]]; then
   exit 1
 fi
 
+if ! [[ -v FLAVOUR ]]; then
+  FLAVOUR=$(basename "$(readlink -f ..)")
+fi
+CONTAINER_IMAGE_NAME=cvmfs-dev__"$FLAVOUR"
+BUILDER_CONTAINER_NAME=cvmfs-dev__"$FLAVOUR"
+WORKER_CONTAINER_NAME_BASE=cvmfs-ci-worker__"$FLAVOUR"-
+
 cd test/common/container
 
 if [[ -v BUILD ]]; then
-  #podman compose up --build -d cvmfs-dev
-  podman build -f Dockerfile-dev . --tag cvmfs-dev-image:clean-slate
+  podman build -f Dockerfile-dev . --tag "$CONTAINER_IMAGE_NAME":clean-slate
   mkdir -p     ../../../../ccache
   chmod -R 777 ../../../../ccache
   mkdir -p     ./build-and-client-tests.tmp
@@ -20,32 +26,32 @@ if [[ -v BUILD ]]; then
   chmod -R 777 ../../../../tmp_cvmfs-build
   mkdir -p     ../../../../tmp_cvmfs-ext
   chmod -R 777 ../../../../tmp_cvmfs-ext
-  podman create --privileged --replace --name cvmfs-dev \
+  podman create --privileged --replace --name "$BUILDER_CONTAINER_NAME" \
     -v /sys/fs/cgroup:/sys/fs/cgroup \
     -v ../../../:/home/sftnight/cvmfs \
     -v ../../../../ccache:/home/sftnight/.ccache \
     -v ./build-and-client-tests.tmp:/tmp \
     -v ../../../../tmp_cvmfs-build:/tmp/cvmfs-build \
     -v ../../../../tmp_cvmfs-ext:/tmp/cvmfs-ext \
-    cvmfs-dev-image:clean-slate
-  podman start cvmfs-dev
+    "$CONTAINER_IMAGE_NAME":clean-slate
+  podman start "$BUILDER_CONTAINER_NAME"
 
-  time podman exec -u sftnight cvmfs-dev bash -c \
+  time podman exec -u sftnight "$BUILDER_CONTAINER_NAME" bash -c \
     "cmake -S /home/sftnight/cvmfs -B /tmp/cvmfs-build -D EXTERNALS_PREFIX=/tmp/cvmfs-ext -D BUILD_SHRINKWRAP=ON"
 
-  time podman exec -u sftnight cvmfs-dev bash -c \
+  time podman exec -u sftnight "$BUILDER_CONTAINER_NAME" bash -c \
     "cd /tmp/cvmfs-build && make -j$(nproc) && sudo make -j$(nproc) install"
 
-  podman commit cvmfs-dev cvmfs-dev-image:build-installed
+  podman commit "$BUILDER_CONTAINER_NAME" "$CONTAINER_IMAGE_NAME":build-installed
 
-  podman exec -u sftnight cvmfs-dev /bin/bash -c "sudo cvmfs_config setup"
-  podman exec -u sftnight cvmfs-dev /bin/bash -c "sudo cvmfs_config chksetup"
+  podman exec -u sftnight "$BUILDER_CONTAINER_NAME" /bin/bash -c "sudo cvmfs_config setup"
+  podman exec -u sftnight "$BUILDER_CONTAINER_NAME" /bin/bash -c "sudo cvmfs_config chksetup"
 
-  podman commit cvmfs-dev cvmfs-dev-image:chksetup
-  podman stop cvmfs-dev
+  podman commit "$BUILDER_CONTAINER_NAME" "$CONTAINER_IMAGE_NAME":chksetup
+  podman stop "$BUILDER_CONTAINER_NAME"
 fi
 
-#time podman rm -f $(podman ps -a --format="{{.Names}}" | grep cvmfs-ci-worker- || true) || true
+#time podman rm -f $(podman ps -a --format="{{.Names}}" | grep "$WORKER_CONTAINER_NAME_BASE" || true) || true
 rm -rf   worker
 
 if ! [[ -v NB_WORKERS ]]; then
@@ -66,15 +72,15 @@ for worker_i in $(seq 1 "$NB_WORKERS"); do
   #podman volume rm var_spool_cvmfs-for-server-tests --force
   #  -v var_spool_cvmfs-for-server-tests:/var/spool/cvmfs \
   #  --tmpfs /var/spool/cvmfs \
-  "${PRIORITIZE[@]}" podman create --ulimit nice=20 --replace --privileged --name cvmfs-ci-worker-$worker_i \
+  "${PRIORITIZE[@]}" podman create --ulimit nice=20 --replace --privileged --name "$WORKER_CONTAINER_NAME_BASE"$worker_i \
     -v /sys/fs/cgroup:/sys/fs/cgroup \
     -v ../../../:/home/sftnight/cvmfs \
     -v ./worker/$worker_i/tmp:/tmp \
     -v ./worker/$worker_i/orders:/orders \
     --tmpfs /var/spool/cvmfs \
-    cvmfs-dev-image:chksetup \
-    && "${PRIORITIZE[@]}" podman start cvmfs-ci-worker-$worker_i \
-    && "${PRIORITIZE[@]}" podman exec -u sftnight cvmfs-ci-worker-$worker_i bash -c \
+    "$CONTAINER_IMAGE_NAME":chksetup \
+    && "${PRIORITIZE[@]}" podman start "$WORKER_CONTAINER_NAME_BASE"$worker_i \
+    && "${PRIORITIZE[@]}" podman exec -u sftnight "$WORKER_CONTAINER_NAME_BASE"$worker_i bash -c \
     "while ! systemctl status &>/dev/null; do sleep 1; done; nohup /home/sftnight/cvmfs/test/common/container/work.sh &> /tmp/work.log &" \
     &
 
@@ -146,7 +152,7 @@ for worker_i in $(seq 1 "$NB_WORKERS"); do
 done
 for worker_i in $(seq 1 "$NB_WORKERS"); do
   if ! [[ -f worker/$worker_i/orders/non-executable-for-quit ]]; then
-    podman stop --ignore cvmfs-ci-worker-$worker_i
+    podman stop --ignore "$WORKER_CONTAINER_NAME_BASE"$worker_i
   fi
 done
 tar -cf - worker/*/tmp/work.log worker/*/tmp/*.job* | zstd -T0 --ultra -20 > worker.$(date +%F_%T).tar.zst
