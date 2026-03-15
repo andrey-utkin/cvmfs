andrey-utkin/cvmfs

commit 0ced8bd29b04cf317472782e5158190906abd5ed

try to fix the bug
The bug is
input->GetIdxInsideChunk() == 0
input->chunk_size() == 0

and the look continuing and NextChunk() not being called.

  do {
    assert(input->IsValid());
    // TODO TODO replace with input->HasInputLeftInChunk()
    if (input->GetIdxInsideChunk() < input->chunk_size()
        && input->chunk_size() != 0) {
      // still stuff to process in the current chunk
    } else if (!input->NextChunk() && !output_full_) {
      return kStreamIOError;

cvmfs/ingestion/task_compress.cc
@@ -45,7 +45,7 @@ void TaskCompress::Process(BlockItem *input_block) {
                    output_block->data(), false);
 
     assert(!output_block->IsFull());
-    assert(in_comp.IsValid());
+    assert(in_comp.IsValid()); // empty InputMem is also valid
     ret_compress = compressor->CompressStream(&in_comp, &out_comp, flush);
     output_block->set_size(out_comp.pos());
 

cvmfs/compression/compressor_echo.cc
@@ -35,20 +35,20 @@ StreamStates EchoCompressor::CompressStream(InputAbstract *input,
     return kStreamError;
   }
 
-  do {
+  while (true) {
     assert(input->IsValid());
-    // TODO TODO replace with input->HasInputLeftInChunk()
-    if (input->GetIdxInsideChunk() < input->chunk_size()
-        && input->chunk_size() != 0) {
-      // still stuff to process in the current chunk
-    } else if (!input->NextChunk() && !output_full_) {
-      return kStreamIOError;
+
+    if (!input->HasInputLeftInChunk()) {
+      bool got_next = input->NextChunk();
+      if (!got_next) {
+        return kStreamEnd;
+      }
+      continue;
     }
 
     const size_t have = input->chunk_size();
-    assert(input->IsValid());
-    assert(output->size() - output->pos() > 0); // we are not trying to write into a full output
     assert(have > 0);
+    assert(output->size() - output->pos() > 0); // we are not trying to write into a full output
     const int64_t written = output->Write(input->chunk(), have);
     if (written < 0) {
       return kStreamIOError;
@@ -61,11 +61,8 @@ StreamStates EchoCompressor::CompressStream(InputAbstract *input,
       return kStreamOutBufFull;
     }
 
-  // TODO TODO replace with input->HasInputLeftInChunk()
-  } while (input->has_chunk_left()
-          || (input->GetIdxInsideChunk() < input->chunk_size()
-              && input->chunk_size() != 0));
-  output_full_ = false;
+  }
+  output_full_ = false; // questionable usefulness of this field
   return kStreamEnd;
 }
 

cvmfs/compression/input_abstract.h
@@ -6,6 +6,7 @@
 #define CVMFS_COMPRESSION_INPUT_ABSTRACT_H_
 
 #include <cstdio>
+#include <cassert>
 
 namespace zip {
 
@@ -90,8 +91,16 @@ class InputAbstract {
   virtual size_t bytes_read() const { return bytes_read_; }
 
   // TODO TODO check if condition is correct or off-by-one error
-  virtual bool HasInputLeftInChunk() const
-             { return idx_inside_chunk_ < chunk_size_ - 1 && chunk_size_ != 0; }
+  virtual bool HasInputLeftInChunk() const {
+    if (chunk_size_ == 0) {
+      return false;
+    }
+    assert(idx_inside_chunk_ <= chunk_size_);
+    if (idx_inside_chunk_ == chunk_size_) {
+      return false;
+    }
+    return true;
+  }
   virtual void SetIdxInsideChunk(const size_t idx) { idx_inside_chunk_ = idx; }
   virtual size_t GetIdxInsideChunk() const { return idx_inside_chunk_; }
 

commit d7b4a80a3f2fc40eec64beae9d732641ac545e32

Hopefully fix TaskCompress::Process() loop exit
The new problem is that certificate (data/hh/hhh....hhhX) ends up empty
because:

 (gdb) print *this
 $2 = (zip::InputMem) {
   <zip::InputAbstract> = {
     _vptr$InputAbstract = 0x78cd38 <vtable for zip::InputMem+16>,
     is_owner_ = false,
     has_chunk_left_ = true,
     max_chunk_size_ = 16384,
     chunk_ = 0x0,
     chunk_size_ = 0,
     idx_inside_chunk_ = 0,
     bytes_read_ = 0,
     static _vtable$ = <optimized out>
   },
   members of zip::InputMem:
   src_ = 0x7fffe7fffb98 "-----BEGIN CERTIFICATE-----\nMIIDDTCCAfWgAwIBAgIUcLbZUm7aRgyoo7UowGSNtxVUUqcwDQYJKoZIhvcNAQEL\nBQAwLzEtMCsGA1UEAwwkdGVzdC5yZXBvIENlcm5WTS1GUyBSZWxlYXNlIE1hbmFn\nZXJzMB4XDTI2MDMxNTE1MTUzMloXDTI3MDMxNTE1MTUzMlowLzEtMCsGA1UEAwwk\ndGVzdC5yZXBvIENlcm5WTS1GUyBSZWxlYXNlIE1hbmFnZXJzMIIBIjANBgkqhkiG\n9w0BAQEFAAOCAQ8AMIIBCgKCAQEA5Ys+/mnATxR6ey9Ru4YQUBEOk65mnQbnknPH\nLUkJ5BfUlWHpzJhMoXo1e5N69S95lrjaqAGpTCylPyXzrB+vwXcErEXycFTNMxPp\nG6pEhskWsk2792gV5anmqco40oImS9iPkm300U0Ji4vEWTWrRVXNNTE33oqJJmB6\nWNg9hW0EyvQ5HsVyx0J0Nq8AxzILUL9yOPGT7LbMfRQNHFQfGSaX7rACxirjcQeG\nIlWK4At1Lhy9835IOm+SAIaCmxHLN702irUzCjxEJGzH1olTm0XSHNuL1ZmAR4zO\nDgGQLvruOgMa09NtD5JPgTPicS8xBgfSW5LmFtyoDrBbiPit8wIDAQABoyEwHzAd\nBgNVHQ4EFgQUGY16i/wHPaHt9PIkQ3ZQgZ8qEiUwDQYJKoZIhvcNAQELBQADggEB\nAI5l//5wwcCgvF+5nR4yIOomRSKl+/aQXotLiQCK2bTdpEKxuS36T8tTfjY8wr74\nuJucZL3mF2FJMTcnCnrKVeXMaiyX6qxUr1A5oZtlvUpSEN629lWIbvC2y6Bef8zF\nhnRdXUK60/1wBXk8Y2P2TGQbg511ZYRJIWf4XfLYfTRSgkTJgki1HL9IiAVaTRTN\n/SdoQdXkZYbpRByaokRskeGPK3Xp0acbWy2CF6W+A9vgzCvV3AfXfd3bg+YLTFl3\nrCXdfarvkXbzn4hzvJZLd+cr3Cu1X61hqBP/NC03JojtO1HiuVuOr5UqMlusSKDm\nweaCN6kNWr+Dfp2gDo32bi8=\n-----END CERTIFICATE-----\n\a",
   src_size_ = 1119,
   idx_ = 18446744073709551615,
   static _vtable$ = <optimized out>
 }
 (gdb) bt
 #0  zip::InputAbstract::HasInputLeftInChunk (this=0x7ffff4806d78) at compression/input_abstract.h:96
 #1  0x0000000000475762 in TaskCompress::Process (this=0x89a2d0, input_block=0x7fffd0000d50) at /usr/local/src/cvmfs/cvmfs/ingestion/task_compress.cc:49
 #2  0x00000000004724f9 in TubeConsumer<BlockItem>::MainConsumer (data=0x89a2d0) at ingestion/task.h:56
 #3  0x00007ffff707f464 in start_thread () from /lib64/libc.so.6
 #4  0x00007ffff71025ec in __clone3 () from /lib64/libc.so.6

Fixes: 0ced8bd29b04

cvmfs/ingestion/task_compress.cc
@@ -47,7 +47,12 @@ void TaskCompress::Process(BlockItem *input_block) {
     assert(!output_block->IsFull());
     assert(in_comp.IsValid()); // empty InputMem is also valid
     if (!in_comp.HasInputLeftInChunk()) {
-      break;
+      if (!in_comp.has_chunk_left()) {
+        break;
+      } else {
+        in_comp.NextChunk();
+        assert(in_comp.IsValid()); // empty InputMem is also valid
+      }
     }
     ret_compress = compressor->CompressStream(&in_comp, &out_comp, flush);
     output_block->set_size(out_comp.pos());