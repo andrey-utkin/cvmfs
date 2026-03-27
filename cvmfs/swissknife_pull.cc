916ab197fe08360bad9edc4cc8a55fae9d5794db
Andriy Utkin <hello@autkin.net>
2026-03-27 17:59:12 +0000

    fixup! Use zip::{Dec,C}ompressorAlgFromEnv() across codebase

--- cvmfs/swissknife_pull.cc
+++ cvmfs/swissknife_pull.cc
@@ -592,8 +592,8 @@ int swissknife::CommandPull::Main(const swissknife::ArgumentList &args) {
   atomic_init64(&overall_new);
   atomic_init64(&chunk_queue);
 
-  decomp = zip::Decompressor::Construct(zip::DecompressorAlgFromEnv());
-  comp = zip::Compressor::Construct(zip::CompressorAlgFromEnv());
+  decomp = zip::Decompressor::Construct(zip::DecompressionAlgFromEnv());
+  comp = zip::Compressor::Construct(zip::CompressionAlgFromEnv());
   copy = zip::Compressor::Construct(zip::kNoCompression);
 
   const bool     follow_redirects = false;
@@ -717,7 +717,8 @@ int swissknife::CommandPull::Main(const swissknife::ArgumentList &args) {
                                             *stratum1_url,
                                             *temp_dir,
                                             download_manager(),
-                                            signature_manager());
+                                            signature_manager(),
+                                            zip::DecompressionAlgFromEnv());
 
       if (!reflog_hash.IsNull()) {
         reflog =
