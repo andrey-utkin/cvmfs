4feebddbd05f7c1a3ad75b92bdf82ab4f723574a
Andriy Utkin <hello@autkin.net>
2026-04-20 19:50:55 +0100

    fixup! fixup! enable passing tuned GuessDecompressor object instead of alg enum value, a mess

--- cvmfs/network/jobinfo.h
+++ cvmfs/network/jobinfo.h
@@ -232,6 +232,7 @@ class JobInfo {
   void SetUrl(const std::string *url) { url_ = url; }
   void SetDecompressor(zip::Algorithm decompressor_alg);
   void SetDecompressor(UniquePtr<zip::Decompressor> decomp);
+  struct ::CacheManager::Label;
   void SetDecompressor(const CacheManager::Label &label);
   void SetProbeHosts(bool probe_hosts) { probe_hosts_ = probe_hosts; }
   void SetHeadRequest(bool head_request) { head_request_ = head_request; }
