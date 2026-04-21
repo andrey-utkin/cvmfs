bdf9fd747712fdea7edbbaba78986fa272314dd0
Andriy Utkin <hello@autkin.net>
2026-04-21 14:52:56 +0100

    fix
    /usr/local/src/cvmfs/cvmfs/object_fetcher.h:312:42: error: no matching member function for call to 'Fetch'
      312 |     return static_cast<DerivedT*>(this)->Fetch(object_hash, file_path, decomp);
          |            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~
    /usr/local/src/cvmfs/cvmfs/object_fetcher.h:137:9: note: in instantiation of member function 'AbstractObjectFetcher<HttpObjectFetcher<catalog::WritableCatalog>
    >::Fetch' requested here
      137 |         Fetch(effective_history_hash, &path,
          |         ^
    /usr/local/src/cvmfs/cvmfs/object_fetcher.h:234:24: note: in instantiation of member function 'AbstractObjectFetcher<HttpObjectFetcher<catalog::WritableCatalog
    >>::FetchHistory' requested here
      234 |     Failures failure = FetchHistory(&raw_history_ptr, history_hash);
          |                        ^
    /usr/local/src/cvmfs/cvmfs/swissknife_migrate.h:344:32: note: in instantiation of member function 'AbstractObjectFetcher<HttpObjectFetcher<catalog::WritableCat
    alog>>::FetchHistory' requested here
      344 |       retval = object_fetcher->FetchHistory(
          |                                ^
    /usr/local/src/cvmfs/cvmfs/swissknife_migrate.cc:171:26: note: in instantiation of function template specialization 'swissknife::CommandMigrate::LoadCatalogs<H
    ttpObjectFetcher<catalog::WritableCatalog>>' requested here
      171 |     loading_successful = LoadCatalogs(manual_root_hash, &fetcher);
          |                          ^
    /usr/local/src/cvmfs/cvmfs/object_fetcher.h:615:12: note: candidate function not viable: no known conversion from 'zip::Decompressor *' to 'zip::DecompressionA
    lg' (aka 'zip::Algorithm') for 3rd argument
      615 |   Failures Fetch(const shash::Any& object_hash, std::string* object_file,
          |            ^
      616 |                  zip::DecompressionAlg decomp_alg) {
          |                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    /usr/local/src/cvmfs/cvmfs/object_fetcher.h:625:12: note: candidate function not viable: requires 4 arguments, but 3 were provided
      625 |   Failures Fetch(const std::string &relative_path,
          |            ^     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      626 |                  zip::DecompressionAlg decomp_alg,
          |                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      627 |                  const bool         nocache,
          |                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~
      628 |                        std::string *file_path) {
          |                        ~~~~~~~~~~~~~~~~~~~~~~

--- cvmfs/object_fetcher.h
+++ cvmfs/object_fetcher.h
@@ -601,17 +601,6 @@ class HttpObjectFetcher :
     return BuildUrl(BuildRelativeUrl(hash));
   }
 
-#if 0
-  Failures Fetch(const shash::Any &object_hash, std::string *object_file) {
-    assert(object_file != NULL);
-    assert(!object_hash.IsNull());
-
-    zip::DecompressionAlg decomp_alg = FIXME;
-    const bool nocache = false;
-    const std::string url = BuildRelativeUrl(object_hash);
-    return Download(url, decomp_alg, nocache, &object_hash, object_file);
-  }
-#endif
   Failures Fetch(const shash::Any& object_hash, std::string* object_file,
                  zip::DecompressionAlg decomp_alg) {
     assert(object_file != NULL);
@@ -627,7 +616,27 @@ class HttpObjectFetcher :
                  const bool         nocache,
                        std::string *file_path) {
     const shash::Any *expected_hash = NULL;
+    UniquePtr<zip::Decompressor> decomp(zip::Decompressor::Construct(decomp_alg));
     return Download(relative_path, decomp_alg, nocache, expected_hash,
+                    file_path);
+  }
+
+  Failures Fetch(const shash::Any& object_hash, std::string* object_file,
+                 zip::Decompressor* decomp) {
+    assert(object_file != NULL);
+    assert(!object_hash.IsNull());
+
+    const bool nocache = false;
+    const std::string url = BuildRelativeUrl(object_hash);
+    return Download(url, decomp, nocache, &object_hash, object_file);
+  }
+
+  Failures Fetch(const std::string &relative_path,
+                 zip::Decompressor* decomp,
+                 const bool         nocache,
+                       std::string *file_path) {
+    const shash::Any *expected_hash = NULL;
+    return Download(relative_path, decomp, nocache, expected_hash,
                     file_path);
   }
 
@@ -641,7 +650,7 @@ class HttpObjectFetcher :
   }
 
   Failures Download(const std::string &relative_path,
-                    zip::DecompressionAlg decomp_alg,
+                    zip::Decompression* decomp,
                     const bool         nocache,
                     const shash::Any  *expected_hash,
                           std::string *file_path) {
@@ -662,7 +671,7 @@ class HttpObjectFetcher :
     const std::string url = BuildUrl(relative_path);
     const bool probe_hosts = false;
     cvmfs::FileSink filesink(f);
-    download::JobInfo download_job(&url, decomp_alg, probe_hosts, expected_hash,
+    download::JobInfo download_job(&url, decomp, probe_hosts, expected_hash,
                                    &filesink);
     download_job.SetForceNocache(nocache);
     download::Failures retval = download_manager_->Fetch(&download_job);
