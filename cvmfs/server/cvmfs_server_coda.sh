1f95537aa09ed9c5056c2aea6ce0d3e24c9c0de8
Andriy Utkin <hello@autkin.net>
2026-06-30 19:34:00 +0100

    server: add CVMFS_SERVER_DEBUG=rr option (alternatively, =4)
    cvmfs_publish is NOT using rr, because that fails currently.

--- cvmfs/server/cvmfs_server_coda.sh
+++ cvmfs/server/cvmfs_server_coda.sh
@@ -145,6 +145,9 @@ if [ $CVMFS_SERVER_DEBUG -ne 0 ]; then
         # do not attach gdb just run debug version
         CVMFS_SERVER_SWISSKNIFE_DEBUG="cvmfs_swissknife_debug"
       ;;
+      4|rr)
+        CVMFS_SERVER_SWISSKNIFE_DEBUG="_RR_TRACE_DIR=/tmp sudo -EP rr record --setuid-sudo cvmfs_swissknife_debug"
+      ;;
     esac
   else
     echo -e "WARNING: compile with CVMFS_SERVER_DEBUG to allow for debug mode!\nFalling back to release mode [cvmfs_swissknife]...."
@@ -162,6 +165,10 @@ if [ $CVMFS_SERVER_DEBUG -ne 0 ]; then
       ;;
       3)
         # do not attach gdb just run debug version
+        CVMFS_SERVER_PUBLISH_DEBUG="/usr/bin/cvmfs_publish_debug"
+      ;;
+      4|rr)
+        # publish shells out to mount tools which fails with fspick being a syscall unsupported by rr
         CVMFS_SERVER_PUBLISH_DEBUG="/usr/bin/cvmfs_publish_debug"
       ;;
     esac
